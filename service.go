c18de89879784479c00d516b5a4ae190a1ddfa2b
Dimas Ananda <traperwaze@gmail.com>
2022-03-23 08:49:37 +0700

    get credential from env
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- service.go
+++ service.go
@@ -1,6 +1,20 @@
 package main
 
-import _ "github.com/joho/godotenv/autoload"
+import (
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var RABBITMQ_HOST = ""
+var RABBITMQ_USER = ""
+var RABBITMQ_PASS = ""
+
+func init() {
+	RABBITMQ_HOST = os.Getenv("RABBITMQ_HOST")
+	RABBITMQ_USER = os.Getenv("RABBITMQ_USER")
+	RABBITMQ_PASS = os.Getenv("RABBITMQ_PASS")
+}
 
 func main() {
 
