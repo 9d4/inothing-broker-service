e0c7bd89dcb2d9cdbf7f55144c42677ae90bc5d1
Dimas Ananda <traperwaze@gmail.com>
2022-04-03 17:00:48 +0700

    continue 21726b55
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- service.go
+++ service.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	_ "github.com/joho/godotenv/autoload"
+)
+
+type part string
+
+const (
+	MQTT part = "MQTT    >> "
 )
 
 var (
@@ -15,58 +19,41 @@ var (
 	RABBITMQ_USER  = ""
 	RABBITMQ_PASS  = ""
 	RABBITMQ_VHOST = ""
+	MQTT_CLIENT_ID = "the-inothing-service"
+	MQTT_SERVER    = ""
 )
+
+var mqttClient mqtt.Client
 
 func init() {
 	RABBITMQ_HOST = os.Getenv("RABBITMQ_HOST")
 	RABBITMQ_USER = os.Getenv("RABBITMQ_USER")
 	RABBITMQ_PASS = os.Getenv("RABBITMQ_PASS")
 	RABBITMQ_VHOST = os.Getenv("VIRTUAL_HOST")
+	MQTT_SERVER = os.Getenv("MQTT_SERVER")
 }
 
 func main() {
-	c := runMqttClient()
-	tick := time.Tick(time.Second)
-
-	for {
-		<-tick
-		fmt.Println(c.IsConnected())
-	}
+	mqttClient = runMqttClient()
 
 	select {}
 }
 
-var (
-	MQTT_CLIENT_ID = "the-inothing-service"
-	MQTT_SERVER    = os.Getenv("MQTT_SERVER")
-)
-
-func mqttMessageHandler(c mqtt.Client, m mqtt.Message) {
-	fmt.Println("HIT")
-}
-
 func runMqttClient() (client mqtt.Client) {
-	mqtt.DEBUG = log.New(os.Stdout, "MQTT:", log.Flags())
-
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(MQTT_SERVER)
 	opts.SetClientID(MQTT_CLIENT_ID)
 	opts.SetUsername(RABBITMQ_USER)
 	opts.SetPassword(RABBITMQ_PASS)
-	opts.SetDefaultPublishHandler(mqttMessageHandler)
+	opts.SetCleanSession(true)
+	opts.SetResumeSubs(true)
 
 	client = mqtt.NewClient(opts)
-	if token:= client.Connect(); token.Wait() && token.Error() != nil {
-		log.Println(token.Error())
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
 	}
 
-	if true {
-		if t := client.Subscribe("#", 0, nil); t.Wait() && t.Error() != nil {
-			log.Println(t.Error())
-			panic("Can't subscribe")
-		}
-	}
-
+	log.Println(MQTT, "Connected!")
 	return
 }
 
