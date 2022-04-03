21726b5587e8ec857eab63eb30e7ecf7d03916a7
Dimas Ananda <traperwaze@gmail.com>
2022-04-03 15:19:38 +0700

    temp: mqtt client which will be used as controller to send msg to things
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- service.go
+++ service.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
+	"time"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	_ "github.com/joho/godotenv/autoload"
 )
 
@@ -21,7 +25,49 @@ func init() {
 }
 
 func main() {
+	c := runMqttClient()
+	tick := time.Tick(time.Second)
 
+	for {
+		<-tick
+		fmt.Println(c.IsConnected())
+	}
+
+	select {}
+}
+
+var (
+	MQTT_CLIENT_ID = "the-inothing-service"
+	MQTT_SERVER    = os.Getenv("MQTT_SERVER")
+)
+
+func mqttMessageHandler(c mqtt.Client, m mqtt.Message) {
+	fmt.Println("HIT")
+}
+
+func runMqttClient() (client mqtt.Client) {
+	mqtt.DEBUG = log.New(os.Stdout, "MQTT:", log.Flags())
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(MQTT_SERVER)
+	opts.SetClientID(MQTT_CLIENT_ID)
+	opts.SetUsername(RABBITMQ_USER)
+	opts.SetPassword(RABBITMQ_PASS)
+	opts.SetDefaultPublishHandler(mqttMessageHandler)
+
+	client = mqtt.NewClient(opts)
+	if token:= client.Connect(); token.Wait() && token.Error() != nil {
+		log.Println(token.Error())
+	}
+
+	if true {
+		if t := client.Subscribe("#", 0, nil); t.Wait() && t.Error() != nil {
+			log.Println(t.Error())
+			panic("Can't subscribe")
+		}
+	}
+
+	return
 }
 
 func CreateNewUser(username string) {
