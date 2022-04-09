6e903aba91356a1fb77673a2d5cbb1b581c3c3b5
Dimas Ananda <traperwaze@gmail.com>
2022-04-09 08:36:06 +0700

    spin up grpc server
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- grpc/server.go
+++ grpc/server.go
@@ -1,1 +1,7 @@
 package grpc
+
+import "github.com/9d4/inothing-broker-service/pb/userpb"
+
+type GrpcServer struct{
+	userpb.UnimplementedUserServiceServer
+}
--- service.go
+++ service.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"log"
+	"net"
 	"os"
 
+	interngrpc "github.com/9d4/inothing-broker-service/grpc"
+	"github.com/9d4/inothing-broker-service/pb/userpb"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	_ "github.com/joho/godotenv/autoload"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type part string
 
 const (
 	MQTT part = "MQTT    >> "
+	GRPC part = "GRPC    >> "
 )
 
 var (
@@ -24,6 +30,8 @@ var (
 )
 
 var mqttClient mqtt.Client
+var grpcListener net.Listener
+var grpcServer *grpc.Server
 
 func init() {
 	RABBITMQ_HOST = os.Getenv("RABBITMQ_HOST")
@@ -34,9 +42,32 @@ func init() {
 }
 
 func main() {
+	grpcListener, grpcServer := startGrpcServer()
 	mqttClient = startMqttClient()
 
+	_, _ = grpcListener, grpcServer
 	select {}
+}
+
+func startGrpcServer() (net.Listener, *grpc.Server) {
+	listener, err := net.Listen("tcp", ":9200")
+	if err != nil {
+		log.Fatal("Can't start listener for grpc server. reason:", err)
+	}
+	server := grpc.NewServer()
+	reflection.Register(server)
+	
+	userpb.RegisterUserServiceServer(server, &interngrpc.GrpcServer{})
+
+	go func() {
+	restart:
+		if err = server.Serve(listener); err != nil {
+			log.Fatal("Can't serve grpc server. reason:", err)
+			goto restart
+		}
+	}()
+	log.Println(GRPC, "Up!")
+	return listener, server
 }
 
 func startMqttClient() (client mqtt.Client) {
